7c83c4b4bc5dd0cd6ec846a1133246892d9b32c6
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-02-23 11:04:44 +0100

    Initial commit

---
+++ src/main/java/it/be/epicode/EsercizioCinque_Progetto/EsercizioCinqueProgettoApplication.java
@@ -0,0 +1,13 @@
+package it.be.epicode.EsercizioCinque_Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioCinqueProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioCinqueProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/be/epicode/EsercizioCinque_Progetto/EsercizioCinqueProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.be.epicode.EsercizioCinque_Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioCinqueProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
